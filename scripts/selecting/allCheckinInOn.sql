04de2e282b8f808f7e5933417251e6306966b127
Code Chrysalis <codechrysalis@Codes-Mac-mini-2.local>
2018-04-20 17:02:44 +0900

    Write select statements for one-to-many

--- scripts/selecting/allCheckinInOn.sql
+++ scripts/selecting/allCheckinInOn.sql
@@ -1,1 +1,5 @@
 --  your SQL here
+SELECT DISTINCT last_name, first_name
+FROM students
+JOIN checkins on students.id = checkins.student_id
+WHERE checked_in_at BETWEEN '20160601' AND '20160630';
--- scripts/selecting/allStudentAndCheckin.sql
+++ scripts/selecting/allStudentAndCheckin.sql
@@ -1,1 +1,3 @@
 -- Your SQL here
+SELECT * FROM students 
+JOIN checkins on students.id = checkins.student_id;
