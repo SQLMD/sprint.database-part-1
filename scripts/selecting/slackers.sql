24bb5596dafa955d28b78631a2a23990443a1937
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-22 00:26:19 +0900

    Completed slackers.sql

--- scripts/selecting/slackers.sql
+++ scripts/selecting/slackers.sql
@@ -1,1 +1,11 @@
--- Your SQL here
+--Using outer join
+select first_name || ' ' || last_name 
+from students 
+left outer join 
+students_to_projects on students_to_projects.student_id = students.id
+WHERE student_id is null;
+
+--using subselect
+select first_name, last_name from students
+WHERE id not in 
+(select student_id from students_to_projects);
