597a7f9af627cecb6f9a7886eace4372b517e9e9
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-22 00:17:34 +0900

    Completed juneProjects query.

--- scripts/selecting/juneProjects.sql
+++ scripts/selecting/juneProjects.sql
@@ -1,1 +1,8 @@
--- Your SQL here
+--  your SQL here
+SELECT DISTINCT projects.name
+FROM students
+INNER JOIN checkins on students.id = checkins.student_id
+INNER JOIN students_to_projects on students_to_projects.student_id = students.id
+INNER JOIN projects on projects.id = students_to_projects.project_id
+WHERE checked_in_at BETWEEN '20160601' AND '20160630'
+order by name;
